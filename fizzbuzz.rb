b0217248716ae7c9494acfd172ccd7c61b2d04e2
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 20:14:55 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+    return "FizzBuzz"
+    
+  elsif num % 3 == 0
+    puts "Fizz"
+    return "Fizz"
+    
+  elsif num % 5 == 0
+    puts "Buzz"
+    return "Buzz"
+    
+  else 
+    puts "nil"
+    return nil
+  end
+end
+
+    
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)    
+  
