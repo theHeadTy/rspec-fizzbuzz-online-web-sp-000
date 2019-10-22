24643b48e42add2f2b86991805d6a2a8b458deaa
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 20:05:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,6 +11,19 @@ def fizzbuzz(num)
       
     elsif num % 5 == 0
     
+      return "Buzz"
+      
+    elsif num % 3 == 0 && num % 5 == 0
+    
+      return "FizzBuzz"
+      
+    else
+      return nil
+    end
+      
+  end
+end
+    
     
     
   
