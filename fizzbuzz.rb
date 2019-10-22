4a3031f23fede2debf55ac35cf7efa4e8f20016c
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 19:57:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,29 +3,5 @@
 
 def fizzbuzz(num)
   
-  if num % 3 == 0 
+  if num % 3 == 0
     puts "Fizz"
-    return "Fizz"
-  end
-  
-  if num % 5 == 0
-    puts "Buzz"
-    return "Buzz"
-  end
-  
-  if (num % 3 && num % 5 == 0)
-    puts "FizzBuzz"
-    return "FizzBuzz"
-  
-  else
-    puts "nil"
-    return nil
-  end
-  
-
-end
-
-fizz_3 = fizzbuzz(3)
-fizz_5 = fizzbuzz(5)
-fizz_15 = fizzbuzz(15)
-fizz_4 = fizzbuzz(4)
